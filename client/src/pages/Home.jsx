d09e318d1d1c696b8cff9e469127d6a2d8baed13
meganirenegott <meganirenegott@gmail.com>
2026-04-07 14:16:10 -0700

    fixed bug in home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,28 @@
 // pages/Home.jsx
 import SuggestionCard from "../components/SuggestionCard.jsx";
 
-function Home({ allUserSuggestions, userByCategory, getSuggestionByCategory }) {
+function Home({ allUserSuggestions, userByCategory, getSuggestionsByCategory }) {
+  const suggestionsToShow =
+    userByCategory.length > 0 ? userByCategory : allUserSuggestions;
 
-    // category display
-    const suggestionsToShow =
-        usersByCategory.length > 0 ? userByCategory : allUserSuggestions;
-    return (
-        <>
-            {/* category buttons */}
-             <div>
-                <button onClick={() => getSuggestionsByCategory("UI")}>UI</button>
-                <button onClick={() => getSuggestionsByCategory("UX")}>UX</button>
-                <button onClick={() => getSuggestionsByCategory("Feature")}>Feature</button>
-            </div>
-            <div className="card-container">
-                {allUserSuggestions && allUserSuggestions.map((uniqueSuggestion, index) => <SuggestionCard uniqueSuggestion={uniqueSuggestion} key={index} />)}
-            </div>
-            <div className="category-suggestion-filter">
-                {userByCategory && userByCategory.map((uniqueSuggestion, index) => <SuggestionCard uniqueSuggestion={uniqueSuggestion} key={index} />)}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <button onClick={() => getSuggestionsByCategory("UI")}>UI</button>
+        <button onClick={() => getSuggestionsByCategory("UX")}>UX</button>
+        <button onClick={() => getSuggestionsByCategory("Feature")}>Feature</button>
+      </div>
+
+      <div className="card-container">
+        {suggestionsToShow.map((uniqueSuggestion, index) => (
+          <SuggestionCard
+            uniqueSuggestion={uniqueSuggestion}
+            key={index}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
-    
-    
-    
-    
-    
-    
-    
-    
+
 export default Home;
